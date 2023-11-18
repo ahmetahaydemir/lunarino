a2d2e1ba25b16d07fbb92b1c3d0c80b4779ff963
Ahmet Taha Aydemir <ahmetahaydemir@gmail.com>
2023-11-18 18:46:26 +0300

    react dnd test

---
+++ app/ui/container.tsx
@@ -0,0 +1,103 @@
+'use client'
+
+import { DndProvider, useDrag } from 'react-dnd'
+import { HTML5Backend } from 'react-dnd-html5-backend'
+import { TouchBackend } from 'react-dnd-touch-backend'
+import type { FC } from 'react'
+import { useCallback, useState } from 'react'
+import update from 'immutability-helper';
+import { Ranking } from './ranking'
+
+export interface RankingData {
+    id: number
+    name: string
+    href: string
+}
+
+export interface ContainerProps {
+    mobile: boolean
+}
+
+let initialToken = false;
+
+export const Container: FC<ContainerProps> = (mobileCheck) => {
+
+    const [rankings, setRankings] = useState([
+        {
+            id: 1,
+            name: "Slay the Spire",
+            href: "/detail"
+        },
+        {
+            id: 2,
+            name: "Risk of Rain 2",
+            href: "/detail"
+        },
+        {
+            id: 3,
+            name: "Vampire Survivors",
+            href: "/detail"
+        },
+        {
+            id: 4,
+            name: "Against the Storm",
+            href: "/detail"
+        },
+        {
+            id: 5,
+            name: "Brotato",
+            href: "/detail"
+        },
+        {
+            id: 6,
+            name: "Dark Souls",
+            href: "/detail"
+        }
+    ])
+
+    const moveRanking = useCallback((dragIndex: number, hoverIndex: number) => {
+        console.log("Movement --> ", dragIndex, "-->", hoverIndex);
+        setRankings((prevRankings: RankingData[]) =>
+            update(prevRankings, {
+                $splice: [
+                    [dragIndex, 1],
+                    [hoverIndex, 0, prevRankings[dragIndex] as RankingData],
+                ],
+            }),
+        )
+    }, [])
+
+    const renderRanking = useCallback(
+        (ranking: { id: number; name: string, href: string }, index: number) => {
+            return (
+                <Ranking
+                    key={ranking.id}
+                    index={index}
+                    id={ranking.id}
+                    name={ranking.name}
+                    href={ranking.href}
+                    moveRanking={moveRanking}
+                />
+            )
+        },
+        [],
+    )
+
+    if (!initialToken) {
+        setTimeout(() => {
+            moveRanking(1, 1);
+        }, 400);
+        initialToken = true;
+    }
+
+    return (
+        <>
+            <div className='w-full h-fit flex flex-col p-4 gap-4 items-center'>
+                <DndProvider backend={mobileCheck.mobile ? TouchBackend : HTML5Backend}>
+                    {rankings.map((rank, i) => renderRanking(rank, i))}
+                </DndProvider>
+            </div>
+        </>
+    )
+
+}
--- app/ui/ranking.tsx
+++ app/ui/ranking.tsx
@@ -1,21 +1,122 @@
+'use client'
 
 import { ChevronDoubleRightIcon } from '@heroicons/react/24/solid'
-import Image from 'next/image'
 import Link from 'next/link'
 
-export default function Ranking(props: any) {
+import { useDrag, useDrop } from 'react-dnd'
+import { useRef, type FC } from 'react'
+import type { Identifier, XYCoord } from 'dnd-core'
 
+export const ItemTypes = {
+    RANKING: 'ranking'
+}
+
+export interface RankingProps {
+    id: any
+    name: string
+    href: string
+    index: number
+    moveRanking: (dragIndex: number, hoverIndex: number) => void
+}
+interface DragItem {
+    index: number
+    id: string
+    type: string
+}
+
+export const Ranking: FC<RankingProps> = ({ id, name, href, index, moveRanking }) => {
+
+    const ref = useRef<HTMLDivElement>(null)
+    //
+    const [{ handlerId }, drop] = useDrop<
+        DragItem,
+        void,
+        { handlerId: Identifier | null }
+    >({
+        accept: ItemTypes.RANKING,
+        collect(monitor) {
+            return {
+                handlerId: monitor.getHandlerId(),
+            }
+        },
+        hover(item: DragItem, monitor) {
+            if (!ref.current) {
+                return
+            }
+            const dragIndex = item.index
+            const hoverIndex = index;
+
+            // Don't replace items with themselves
+            if (dragIndex === hoverIndex) {
+                return
+            }
+
+            // Determine rectangle on screen
+            const hoverBoundingRect = ref.current?.getBoundingClientRect()
+
+            // Get vertical middle
+            const hoverMiddleY =
+                (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2
+
+            // Determine mouse position
+            const clientOffset = monitor.getClientOffset()
+
+            // Get pixels to the top
+            const hoverClientY = (clientOffset as XYCoord).y - hoverBoundingRect.top
+
+            // Only perform the move when the mouse has crossed half of the items height
+            // When dragging downwards, only move when the cursor is below 50%
+            // When dragging upwards, only move when the cursor is above 50%
+
+            // Dragging downwards
+            if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
+                return
+            }
+
+            // Dragging upwards
+            if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
+                return
+            }
+
+            // Time to actually perform the action
+            moveRanking(dragIndex, hoverIndex)
+
+            // Note: we're mutating the monitor item here!
+            // Generally it's better to avoid mutations,
+            // but it's good here for the sake of performance
+            // to avoid expensive index searches.
+            item.index = hoverIndex
+        },
+    })
+    //
+    const [{ isDragging }, drag] = useDrag({
+        type: ItemTypes.RANKING,
+        item: () => {
+            return { id, index }
+        },
+        collect: (monitor: any) => ({
+            isDragging: monitor.isDragging(),
+        }),
+    })
+    //
+    drag(drop(ref))
+    //
     return (
-        <Link
-            key={props.id}
-            href={props.href}
-            className="ranking flex shadow-md h-[48px] items-center justify-center gap-2 rounded-md font-medium hover:border-indigo-300"
+        <div ref={ref}
+            data-handler-id={handlerId}
+            className={isDragging ? 'opacity-0 w-full' : 'opacity-100 w-full'}
         >
-            <div className='flex w-[54px] h-[54px] shadow-md bg-blue-200 text-black justify-center text-xl rounded-2xl'>
-                <b className='self-center'>{props.id}</b>
-            </div>
-            <ChevronDoubleRightIcon className="w-8 fill-blue-200" />
-            <p className="w-full text-slate-100 text-lg">{props.name}</p>
-        </Link>
+            <Link
+                key={id}
+                href={href}
+                className="ranking flex shadow-md h-[48px] items-center justify-center gap-2 rounded-md font-medium hover:border-indigo-300"
+            >
+                <div className='flex w-[54px] h-[54px] shadow-md bg-blue-200 text-black justify-center text-xl rounded-2xl'>
+                    <b className='self-center'>{id}</b>
+                </div>
+                <ChevronDoubleRightIcon className="w-8 fill-blue-200" />
+                <p className="w-full text-slate-100 text-lg">{name}</p>
+            </Link>
+        </div>
     )
 }
